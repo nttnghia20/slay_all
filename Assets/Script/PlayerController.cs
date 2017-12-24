78cb669b3dc1cd81d9efb13fa0effe1f4cc22bb6
Dante <nttnghia20@gmail.com>
2017-12-24 23:04:13 +0700

    *Update PlayerController.cs

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float smooth = 2.0F;
+    public float turnSpeed = 30.0F;
+
+    private Rigidbody2D rd2d;
+
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        /*
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rd2d.AddForce(movement * speed); */
+
+
+        //Rotate
+        /*float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);*/
+
+        
+       
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime);
+    }
+}
